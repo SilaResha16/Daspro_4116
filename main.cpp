958269a7a0af948b81ade7fb9470c3910cfb7239
SilaResha16 <113013769+SilaResha16@users.noreply.github.com>
2022-09-20 00:37:37 -0700

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <stdio.h>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello world!" << endl;
+
+
+
+
+    int u = 18;
+    float t = 162.5;
+    char ub = 'M';
+    string ubs = "XL";
+
+    cout << "Umur saya : " << u;
+    cout << endl;
+    cout << "Tinggi saya : " << t;
+    cout << "\nUkuran baju : " << ub;
+    cout << "\nUkuran baju : " << ubs;
+
+    int a;
+    cout << "\nMasukan angka : 9";
+    cin >> a;
+    cout << "\nAngka" << endl;
+    return 0;
+}
