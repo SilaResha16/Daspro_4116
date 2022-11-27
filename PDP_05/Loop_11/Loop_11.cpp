e1ef496be25736d7f8140679b583028b9bf65551
SilaResha16 <113013769+SilaResha16@users.noreply.github.com>
2022-11-27 21:36:28 +0700

    Add files via upload

---
+++ PDP_05/Loop_11/Loop_11.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+   for (int x = 1; x < 11; x++){
+       int y = x*7;
+       cout << x << " x " << 7 << " = " << y << endl;
+   }
+    return 0;
+}
